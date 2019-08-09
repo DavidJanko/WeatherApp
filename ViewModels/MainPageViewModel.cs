3b31f5529e9d29ca74100b34df84cabcc4474a72
DavidJanko <davidjanko@pslib.cz>
2019-08-10 00:30:29 +0200

    Přidání Commandů + INotifyPropertyChange + Vytvoření databáze

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -7,32 +7,38 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using WeatherApp.ViewModels.Models;
+using Xamarin.Forms;
 
 namespace WeatherApp.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : Abstract.Abstract
     {
-        private string _nejakejText;
+        private string vstupniText;
+
+        public Command Vyhledat { get; set; }
+        public Command HledatGPS { get; set; }
 
         public MainPageViewModel()
         {
-            NejakejText = "Už nechci umřít";
+            Vyhledat = new Command(GetWeatherData);
+            HledatGPS = new Command(NajitLokaci);
+        }
+        public string VstupniText { get; set; }
+
+        private void NajitLokaci()
+        {
+
         }
 
-        public string NejakejText { get { return _nejakejText; } set { _nejakejText = value; GetWeatherData("Liberec"); } }
-
-        private WeatherData GetWeatherData(string mesto)
+        private void GetWeatherData()
         {
-            if (mesto == null)
+            if (VstupniText == null)
             {
-                return null;
             }
             else
             {
-                var json = new WebClient().DownloadString("http://api.openweathermap.org/data/2.5/weather?q=" + mesto + "&APPID=fedcf81469d34db86a7e24886bb9ae83");
+                var json = new WebClient().DownloadString("http://api.openweathermap.org/data/2.5/weather?q=" + VstupniText + "&APPID=fedcf81469d34db86a7e24886bb9ae83");
                 var data = JsonConvert.DeserializeObject<WeatherData>(json);
-
-                return data;
             }
         }
     }
---
+++ ViewModels/Models/Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.ViewModels.Models
+{
+    public static class Database
+    {
+
+    }
+}
