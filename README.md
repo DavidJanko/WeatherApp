cdb3d0df3458d3f72957e745b56c2c417c435669
DavidJanko <davidjanko@pslib.cz>
2019-08-09 23:12:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WeatherApp
+ Weather aplikace pro android (fuj ios)
---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            NejakejText = "Už nechci umřít";
+        }
+
+        public string NejakejText { get; set; }
+    }
+}
